3b886ffdaac73bea5d686ace06490cd0f2efe331
Cristian Molina <cmutep@gmail.com>
2022-04-20 00:38:28 -0600

    cant type anything that's not a number

--- index.php
+++ index.php
@@ -95,44 +95,69 @@
 									<div class="form-group">
 										<h5  class="card-title">Service Time</h5>
 										<div class=" input-group">
-											<input type="number" class="form-control font-weight-light" placeholder="Years (must be a number)">
-											<span class="input-group-addon">__</span>
-											<input type="number" class="form-control font-weight-light" placeholder="Months (must be a number)">
+											<input id="service_time" type="number" min="1" step="1" pattern="^[-/d]/d*$" class="form-control font-weight-light" placeholder="Years (must be a whole number)">
+											<span class="input-group-addon">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+											<input id="months" type="number" min="1" step="1" pattern="^[-\d]\d*$" class="form-control font-weight-light" placeholder="Months (must be a whole number)">
 										</div>
 									</div>
 					<!--/Service Time Input -->
 
 										<!-- monthly salary input -->
-										<h5  class="card-title">Monthly Salary</h5>
-											<input type="number" class="form-control font-weight-light" placeholder="Salary (must be a number)"><br>
+										<h5 class="card-title">Monthly Salary</h5>
+											<input id="monthly_salary" type="number" min="1" step="1" pattern="^[-\d]\d*$" class="form-control font-weight-light" placeholder="Salary (must be a whole number)"><br>
 										<!-- /monthly salary input -->
 
 										<!-- age at retirement input -->
-										<h5  class="card-title">Age at Retirement</h5>
-											<input type="number" class="form-control font-weight-light" placeholder="Age (must be a number)">
+										<h5 class="card-title">Age at Retirement</h5>
+											<input id="age_at_retirement" type="number" min="1" step="1" pattern="^[-\d]\d*$" class="form-control font-weight-light" placeholder="Age (must be a whole number)"><br>
 				    					<!-- age at retirement input -->
 
-				    						<!-- radio buttons -->
-										<h5  class="card-title">Marital Status</h5>
+				    					<!-- radio buttons (marital status) -->
+										<h5  id="marital_status" class="card-title">Marital Status</h5>
 
-										<div class="custom-control custom-radio mb-2">
-											<input type="radio" class="custom-control-input" name="cr-l" id="cr_l_s_s" checked>
-											<label class="custom-control-label" for="cr_l_s_s">Single</label>
-										</div>
+										<div class="border p-3 rounded">
+											<div class="custom-control custom-radio mb-2">
+												<input type="radio" min="1" step="1" class="custom-control-input" name="cr-l" id="cr_l_s_s" checked="">
+												<label class="custom-control-label" for="cr_l_s_s">Single</label>
+											</div>
 
-										<div class="custom-control custom-radio mb-3">
-											<input type="radio" class="custom-control-input" name="cr-l" id="cr_l_s_u">
-											<label class="custom-control-label" for="cr_l_s_u">Married</label>
-										</div>
+											<div class="custom-control custom-radio mb-3">
+												<input type="radio" min="1" step="1" class="custom-control-input" name="cr-l" id="cr_l_s_u">
+												<label class="custom-control-label" for="cr_l_s_u">Married</label>
+											</div>
+										</div><br>
 										<!-- /radio buttons -->
 
 										<!-- age at retirement input -->
 										<h5  class="card-title">Spouse's Age at Retirement</h5>
-											<input type="number" class="form-control font-weight-light" placeholder="Spouse's Age (must be a number)"><br>
+											<input id="spouse_age_at_retirement" type="number" pattern="^[-\d]\d*$" min="1" step="1" class="form-control font-weight-light" placeholder="Spouse's Age (must be a whole number)"><br>
 										<!-- /age at retirement input -->	
-									 
+
+										<!-- radio buttons (optional settlement) -->
+										<h5  id="optional_settlement" class="card-title">Optional Settlement</h5>
+										<div class="border p-3 rounded">
+											<div class="custom-control custom-radio mb-2">
+												<input type="radio" class="custom-control-input" name="cr_l_s" id="cr_lr_s_s" checked="">
+												<label class="custom-control-label" for="cr_lr_s_s">Joint & 2/3 (Normal Form)</label>
+											</div>
+
+											<div class="custom-control custom-radio mb-3">
+												<input type="radio" class="custom-control-input" name="cr_l_s" id="cr_lr_s_u">
+												<label class="custom-control-label" for="cr_lr_s_u">Joint & 100%</label>
+											</div>
+											<div class="custom-control custom-radio mb-2">
+												<input type="radio" class="custom-control-input" name="cr_l_s" id="cr_lr_s_s_s">
+												<label class="custom-control-label" for="cr_lr_s_s_s">Joint & 50%</label>
+											</div>
+
+											<div class="custom-control custom-radio mb-3">
+												<input type="radio" class="custom-control-input" name="cr_l_s" id="cr_lr_s_u_u">
+												<label class="custom-control-label" for="cr_lr_s_u_u">Life Only</label>
+											</div>
+
+											</div>
 										</div>
-									</div>
+
 								</form> 
 							</div>
 					<!-- /select2 advanced -->   
@@ -144,6 +169,29 @@
 
 		</div>
 		<!-- /main content -->
+
+		<script>
+			//don't allow the user to enter anything other than a number
+				{
+				const
+				intRx = /\d/,
+				integerChange = (event) => {
+					if (
+					(event.key.length > 1) ||
+					( (event.key === "-") && (!event.currentTarget.value.length) ) ||
+					intRx.test(event.key)
+					) return; 
+					let target = event.currentTarget.id;
+					console.log(target)
+					$("#"+String(target)).fadeTo(100, 0.3, function() { $(this).fadeTo(500, 1.0); });
+					event.preventDefault();
+				};
+				for (let input of document.querySelectorAll(
+				'input[type="number"][step="1"]'
+				)) input.addEventListener("keydown", integerChange);
+
+				}
+		</script>
 
 
 
