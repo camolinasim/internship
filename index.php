cd6b187a1fbf49afe93fdc88112be18ab666de05
Cristian Molina <cmutep@gmail.com>
2022-05-04 23:57:38 -0600

    can read form

--- index.php
+++ index.php
@@ -42,7 +42,6 @@
 		<div class="content-wrapper">
 			<!-- City of El Paso Picture -->
 			<div class="content pt0">
-							<!-- Form inputs -->
 							<div class="card parent">
 						<div class="card-header">
 						<div>
@@ -56,7 +55,7 @@
 
 					<!-- Select2 selects -->
 						<div class="col-lg-6">
-						<form action="#">
+						<form action="test.php" method="get">
 
 							<div class="card">
 								<div class="card-body"  style="overflow-y:auto;">
@@ -65,8 +64,8 @@
 									<div class="form-group">
 										<select class="form-control select" data-fouc>
 											<optgroup label="">
-												<option value="type1">Before September 1, 2011 (Tier I)</option>
-												<option value="type2">On or After September 1, 2011 (Tier II)</option>
+												<option name="before_september1" value="type1">Before September 1, 2011 (Tier I)</option>
+												<option name="after_september1"value="type2">On or After September 1, 2011 (Tier II)</option>
 										</select>
 
 					<!-- Service Time Input -->
@@ -75,21 +74,21 @@
 									<div class="form-group">
 										<h5  class="card-title">Service Time</h5>
 										<div class=" input-group">
-											<input id="service_time" type="number" min="1" step="1" pattern="^[-/d]/d*$" class="form-control font-weight-light" placeholder="Years (must be a whole number)">
+											<input id="service_time" name="service_time" type="number" min="1" step="1" pattern="^[-/d]/d*$" class="form-control font-weight-light" placeholder="Years (must be a whole number)">
 											<span class="input-group-addon">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-											<input id="months" type="number" min="1" step="1" pattern="^[-\d]\d*$" class="form-control font-weight-light" placeholder="Months (must be a whole number)">
+											<input id="months" name="months" type="number" min="1" step="1" pattern="^[-\d]\d*$" class="form-control font-weight-light" placeholder="Months (must be a whole number)">
 										</div>
 									</div>
 					<!--/Service Time Input -->
 
 										<!-- monthly salary input -->
 										<h5 class="card-title">Monthly Salary</h5>
-											<input id="monthly_salary" type="number" min="1" step="1" pattern="^[-\d]\d*$" class="form-control font-weight-light" placeholder="Salary (must be a whole number)"><br>
+											<input id="monthly_salary"name="monthly_salary" type="number" min="1" step="1" pattern="^[-\d]\d*$" class="form-control font-weight-light" placeholder="Salary (must be a whole number)"><br>
 										<!-- /monthly salary input -->
 
 										<!-- age at retirement input -->
 										<h5 class="card-title">Age at Retirement</h5>
-											<input id="age_at_retirement" type="number" min="1" step="1" pattern="^[-\d]\d*$" class="form-control font-weight-light" placeholder="Age (must be a whole number)"><br>
+											<input id="age_at_retirement" name="age_at_retirement" type="number" min="1" step="1" pattern="^[-\d]\d*$" class="form-control font-weight-light" placeholder="Age (must be a whole number)"><br>
 				    					<!-- age at retirement input -->
 
 				    					<!-- radio buttons (marital status) -->
@@ -97,46 +96,47 @@
 
 										<div class="border p-3 rounded">
 											<div class="custom-control custom-radio mb-2">
-												<input type="radio" min="1" step="1" class="custom-control-input" name="cr-l" id="cr_l_s_s" checked="">
-												<label class="custom-control-label" for="cr_l_s_s">Single</label>
+												<input type="radio" min="1" step="1" class="custom-control-input" name="marital_radio" id="Single" checked="">
+												<label class="custom-control-label" for="Single">Single</label>
 											</div>
 
 											<div class="custom-control custom-radio mb-3">
-												<input type="radio" min="1" step="1" class="custom-control-input" name="cr-l" id="cr_l_s_u">
-												<label class="custom-control-label" for="cr_l_s_u">Married</label>
+												<input type="radio" min="1" step="1" class="custom-control-input" name="marital_radio" id="Married">
+												<label class="custom-control-label" for="Married">Married</label>
 											</div>
 										</div><br>
 										<!-- /radio buttons -->
 
 										<!-- age at retirement input -->
 										<h5  class="card-title">Spouse's Age at Retirement</h5>
-											<input id="spouse_age_at_retirement" type="number" pattern="^[-\d]\d*$" min="1" step="1" class="form-control font-weight-light" placeholder="Spouse's Age (must be a whole number)"><br>
+											<input id="spouse_age_at_retirement" name="spouse_age_at_retirement" type="number" pattern="^[-\d]\d*$" min="1" step="1" class="form-control font-weight-light" placeholder="Spouse's Age (must be a whole number)"><br>
 										<!-- /age at retirement input -->	
 
 										<!-- radio buttons (optional settlement) -->
-										<h5  id="optional_settlement" class="card-title">Optional Settlement</h5>
+										<h5 class="card-title">Optional Settlement</h5>
 										<div class="border p-3 rounded">
 											<div class="custom-control custom-radio mb-2">
-												<input type="radio" class="custom-control-input" name="cr_l_s" id="cr_lr_s_s" checked="">
-												<label class="custom-control-label" for="cr_lr_s_s">Joint & 2/3 (Normal Form)</label>
+												<input type="radio" class="custom-control-input" name="optional_settlement" id="joint2_3" checked="">
+												<label class="custom-control-label" for="joint2_3">Joint & 2/3 (Normal Form)</label>
 											</div>
 
 											<div class="custom-control custom-radio mb-3">
-												<input type="radio" class="custom-control-input" name="cr_l_s" id="cr_lr_s_u">
-												<label class="custom-control-label" for="cr_lr_s_u">Joint & 100%</label>
+												<input type="radio" class="custom-control-input" name="optional_settlement" id="joint100">
+												<label class="custom-control-label" for="joint100">Joint & 100%</label>
 											</div>
 											<div class="custom-control custom-radio mb-2">
-												<input type="radio" class="custom-control-input" name="cr_l_s" id="cr_lr_s_s_s">
-												<label class="custom-control-label" for="cr_lr_s_s_s">Joint & 50%</label>
+												<input type="radio" class="custom-control-input" name="optional_settlement" id="joint50">
+												<label class="custom-control-label" for="joint50">Joint & 50%</label>
 											</div>
 
 											<div class="custom-control custom-radio mb-3">
-												<input type="radio" class="custom-control-input" name="cr_l_s" id="cr_lr_s_u_u">
-												<label class="custom-control-label" for="cr_lr_s_u_u">Life Only</label>
+												<input type="radio" class="custom-control-input" name="optional_settlement" id="life_only">
+												<label class="custom-control-label" for="life_only">Life Only</label>
 											</div>
 
 											</div>
 										</div>
+										<input type="submit">
 
 								</form> 
 							</div>
